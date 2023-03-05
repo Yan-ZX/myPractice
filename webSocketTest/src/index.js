c19498aee03fe23cf0b428b64e6335dd701ea683
yanzx-c <yanzx-c@glodon.com>
2023-03-05 21:49:10 +0800

    完成koa+webSocket练习

--- webSocketTest/src/index.js
+++ webSocketTest/src/index.js
@@ -1,8 +1,40 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router')
+const websockify = require('koa-websocket')
+const app = websockify(new Koa())
+const router = new Router()
+
+app.ws.use((ctx, next) => {
+  return next(ctx)
+})
+
+router.get('/', async ctx => {
+  ctx.body = '欢迎'
+})
+
+router.all('/websocket/:id', async ctx => {
+  let t = setInterval(function() {
+    let n = Math.random()
+    if(n > 0.3) {
+      let msg = JSON.stringify({ 'id': ctx.params.id, 'n': n })
+      ctx.websocket.send(msg)
+    }
+  }, 1000)
+  ctx.websocket.on('message', msg => {
+    console.log('前端发过来的数据：', `${ msg }`)
+  })
+  ctx.websocket.on('close', () => {
+    console.log('前端关闭了websocket')
+  })
+})
+
+app
+.ws
+.use(router.routes())
+.use(router.allowedMethods())
 
 app.use(async ctx => {
-  ctx.body = 'Hello World';
+  ctx.body = 'Hello yanzx';
 });
 
 app.listen(3000, () => {
