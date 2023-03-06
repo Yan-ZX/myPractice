ccd7e740df8930b4eb02a23fc97415ee19d53428
yanzx-c <yanzx-c@glodon.com>
2023-03-06 21:54:56 +0800

    完成单例模式练习

--- designTest/single.js
+++ designTest/single.js
@@ -1,1 +1,21 @@
-console.log('my single');
+class Singleton {
+  constructor(name) {
+    this._name = name;
+  }
+
+  static getSingleTon(name) {
+    if (!this.singleton) {
+      this.singleton = new Singleton(name);
+    }
+    return this.singleton;
+  }
+  get name() {
+    return this._name;
+  }
+}
+
+let a = Singleton.getSingleTon('a1');
+let b = Singleton.getSingleTon('b1');
+
+console.log(a === b); // true
+console.log(a.name); // a1
